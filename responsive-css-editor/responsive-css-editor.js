ec9467c836527603f024931b51aac57101aa51bb
Tyler Thomas (KMVG) <tyler@kmvg.org>
2024-09-15 23:31:15 -0400

    Update responsive-css-editor.js
    Inclusion of CodeMirror

--- responsive-css-editor/responsive-css-editor.js
+++ responsive-css-editor/responsive-css-editor.js
@@ -1,24 +1,46 @@
 jQuery(document).ready(function($) {
-   $('#responsive-css-editor-tabs').tabs();
+    $('#responsive-css-editor-tabs').tabs();
 
-   // Save button functionality
-   $('#save-css-button').click(function() {
-       var desktopCSS = $('#desktop-css-code').val();
-       var tabletCSS = $('#tablet-css-code').val();
-       var mobileCSS = $('#mobile-css-code').val();
+    // Initialize CodeMirror for each textarea
+    var desktopEditor = CodeMirror.fromTextArea(document.getElementById('desktop-css-code'), {
+        lineNumbers: true,
+        mode: "css",
+        theme: "monokai"
+    });
 
-       $.ajax({
-           url: ajaxurl,
-           type: 'POST',
-           data: {
-               action: 'save_responsive_css',
-               desktop_css: desktopCSS,
-               tablet_css: tabletCSS,
-               mobile_css: mobileCSS,
-           },
-           success: function(response) {
-               alert('CSS Saved!');
-           }
-       });
-   });
+    var tabletEditor = CodeMirror.fromTextArea(document.getElementById('tablet-css-code'), {
+        lineNumbers: true,
+        mode: "css",
+        theme: "monokai"
+    });
+
+    var mobileEditor = CodeMirror.fromTextArea(document.getElementById('mobile-css-code'), {
+        lineNumbers: true,
+        mode: "css",
+        theme: "monokai"
+    });
+
+    // Save button functionality
+    $('#save-css-button').click(function() {
+        var desktopCSS = desktopEditor.getValue();
+        var tabletCSS = tabletEditor.getValue();
+        var mobileCSS = mobileEditor.getValue();
+
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'save_responsive_css',
+                desktop_css: desktopCSS,
+                tablet_css: tabletCSS,
+                mobile_css: mobileCSS,
+            },
+            success: function(response) {
+                alert('CSS Saved!');
+            },
+            error: function(response) {
+                alert('Error saving CSS.');
+            }
+        });
+    });
 });
